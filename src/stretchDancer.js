f1f8a79c81e1f75123637e0a034239f04369cf63
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-06-09 20:18:37 -0700

    add jumpy dancer

--- src/stretchDancer.js
+++ src/stretchDancer.js
@@ -1,25 +1,23 @@
-var JumpyDancer = function(top, left, timeBetweenSteps){
+var StretchDancer = function(top, left, timeBetweenSteps){
   // this = Object.create(BlinkyDancer.prototype);
   this._oldStep = Dancer.prototype.step; 
   
   Dancer.apply(this, [top, left, timeBetweenSteps]);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this func   tion
-  this._toggle = true; 
+
 }; 
 
-JumpyDancer.prototype = Object.create(Dancer.prototype); 
-JumpyDancer.prototype.constructor = BlinkyDancer;  
+StretchDancer.prototype = Object.create(Dancer.prototype); 
+StretchDancer.prototype.constructor = BlinkyDancer;  
 
-JumpyDancer.prototype.step = function(){ 
+StretchDancer.prototype.step = function(){ 
   this._oldStep.call(this); 
-  var oldPos = this.$node.position(); 
-  var newPos = {}
   if( this._toggle ) { 
-    newPos["top"] = oldPos.top + 10;  
+    newPos["top"] = oldPos.left + 10;  
   } else { 
-    newPos["top"] = oldPos.top - 10; 
+    newPos["top"] = oldPos.left - 10; 
   }
   this._toggle = !this._toggle;      
-  this.$node.css(newPos); 
+  this.$node.transform(newPos); 
 };
